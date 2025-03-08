2ba9cd8c9ab8af9fd6c777cbaea6ba0ac000cd40
apprehen <3514392356@qq.com>
2025-03-08 17:55:05 +0800

    feat: 尝试使用树形控件模拟文件目录

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 ## 例子
 
+<<<<<<< Updated upstream
 https://svelte.dev/tutorial/kit/introducing-sveltekit
+=======
+https://svelte.dev/tutorial/kit/introducing-sveltekit
+>>>>>>> Stashed changes
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { useRef } from 'react';
 import Editor from '@monaco-editor/react';
 import styles from './index.module.less';
@@ -66,3 +67,51 @@ export default function HomePage() {
     </>
   );
 }
+=======
+import { useRef } from 'react'
+import Editor from '@monaco-editor/react'
+import styles from './index.module.less'
+import { useState } from 'react'
+import type { MenuProps } from 'antd'
+import { Layout, Tree } from 'antd'
+import { treeData } from '@/data/index.tsx'
+
+const { Sider } = Layout
+
+type MenuItem = Required<MenuProps>['items'][number]
+
+export default function HomePage() {
+  const [collapsed, setCollapsed] = useState(false)
+
+  const editorRef = useRef(null)
+
+  function handleEditorDidMount(editor, monaco) {
+    editorRef.current = editor
+  }
+
+  function showValue() {
+    alert(editorRef.current.getValue())
+  }
+  const onSelect = (selectedKeys: React.Key[], info: any) => {
+    console.log('selected', selectedKeys, info)
+  }
+  return (
+    <>
+      <Layout className={styles.layout}>
+        <Sider>
+          <Tree
+            showLine
+            showIcon
+            defaultExpandedKeys={['0-0-0']}
+            onSelect={onSelect}
+            treeData={treeData}
+          />
+        </Sider>
+        <Layout>
+          <Editor></Editor>
+        </Layout>
+      </Layout>
+    </>
+  )
+}
+>>>>>>> Stashed changes
